3c82adff8b8784d0b8fdbe0e5619c4ea95d7f43d
vinodt17 <58369428+vinodt17@users.noreply.github.com>
2020-09-26 23:20:02 +0530

    Delete predict_premium.py

--- Data_Preprocessing/Prediction/predict_premium.py
+++
@@ -1,8 +0,0 @@
-
-class prediction:
-    def __init__(self,model,data):
-        self.model=model
-        self.data=data
-
-    def prediction(self):
-        return self.model.prediction(self.data)
